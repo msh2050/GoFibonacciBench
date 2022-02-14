9e5d0fed06e959dff079870f1fbe7e6ecf00b795
Mustafa Salman <msh2050@yahoo.com>
2022-02-14 14:41:19 +0300

    add table upto fib(1025)

---
+++ creatfibtable.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+func creatfibtable() {
+	f, err := os.Create("fibtable/fibtable.txt")
+	check(err)
+	defer func(f *os.File) {
+		err := f.Close()
+		check(err)
+	}(f)
+
+	w := bufio.NewWriter(f)
+	for i := 0; i < 1000; i++ {
+
+		a := fibonacciGoroutine(i).Bytes()
+		semiformat := fmt.Sprintf("%v", a)
+		semiformat = strings.ReplaceAll(semiformat, "[", "{")
+		semiformat = strings.ReplaceAll(semiformat, "]", "}")
+		tokens := strings.Split(semiformat, " ")
+		_, err = fmt.Fprint(w, strings.Join(tokens, ", "))
+		check(err)
+		_, err = fmt.Fprintf(w, " , ")
+		check(err)
+
+	}
+
+	err = w.Flush()
+	check(err)
+}
